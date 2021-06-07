39f77f02a2c20e11eaf61f334a10ad688dc724b6
alla karpenko <karpenkoallo4ka@gmail.com>
2021-06-07 17:35:14 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,72 +1,50 @@
-import React from 'react';
-import  { Component } from 'react';
-import ContactList from './components/ContactList';
-import ContactForm from './components/ContactForm';
-import Filter from './components/Filters';
+import React from "react";
+import { Component } from "react";
+import ContactList from "./components/ContactList";
+import ContactForm from "./components/ContactForm";
+import Filter from "./components/Filters";
 
-import './App.scss';
+import "./App.scss";
 
 class App extends Component {
-    state = {
-        contacts: [],
-        filter: '',
-    }
-    
-    handleSubmitForm = (contact) => {
-        this.setState(({contacts}) => ({
-            contacts: [...contacts, contact],
-        }));
-    }
-    
-    checkContact = ( name ) => {
-        const { contacts } = this.state
-        
-        const checked = !!contacts.find(
-        (contact) => contact.name === name);
-  
-        checked && alert(`${name} is already in contacts`)
-        return !checked
-      
-    };
-    
-    componentDidMount(){
-        const contacts = localStorage.getItem('contacts');
-        const parsetContact = JSON.parse(contacts);
-        if (parsetContact){
-            this.setState({ contacts: parsetContact });
-        }
-    }
+  state = {
+    contacts: [],
+    filter: "",
+  };
 
-    componentDidUpdate(prevProps, prevState) {
-        if(this.state.contacts !== prevState.contacts) {
-            localStorage.setItem( 'contacts', JSON.stringify(this.state.contacts));
-        }
-    }
+  handleSubmitForm = (contact) => {
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, contact],
+    }));
+  };
 
-    render() { 
-        const { contacts} = this.state;
+  checkContact = (name) => {
+    const { contacts } = this.state;
 
-        return (
-            <div className='Phonebook'>
-               <h1 className='Phonebook-title'>Phonebook</h1>
-                <ContactForm    onSubmit={this.handleSubmitForm} check={this.checkContact} />
-          
-                <h2>Contacts</h2>
-                {contacts.length > 1 && (
-                    <Filter />
-                )}
+    const checked = !!contacts.find((contact) => contact.name === name);
 
+    checked && alert(`${name} is already in contacts`);
+    return !checked;
+  };
 
-                {contacts.length > 0 && (
-                    <ContactList  />
-                )}
-            </div>
-        );
-    }
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div className="Phonebook">
+        <h1 className="Phonebook-title">Phonebook</h1>
+        <ContactForm
+          onSubmit={this.handleSubmitForm}
+          check={this.checkContact}
+        />
+        <h2>Contacts</h2>
+
+        <Filter />
+
+        <ContactList />
+      </div>
+    );
+  }
 }
- 
+
 export default App;
-
-
-
-
